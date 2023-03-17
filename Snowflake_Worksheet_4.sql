b01ee8f9a7bfb12d37db9a635d81d8ca46501227
Balaji Kottana <122858293+BalajiKottana@users.noreply.github.com>
2023-03-17 17:07:33 -0400

    Update Snowflake_Worksheet_4.sql

--- Snowflake_Worksheet_4.sql
+++ Snowflake_Worksheet_4.sql
@@ -1,1 +1,17 @@
---dsfasdfasd
+/*-----------------------------------------------------------------------------------------
+If the previous session is not retained execute the first three sql commands
+--------------------------------------------------------------------------------------------*/
+-- USE ROLE system admin to run the script
+USE ROLE SYSADMIN;
+
+USE WAREHOUSE ELT_WH;
+
+-- Switch default context to the LAB_DB and PUBLIC schema
+USE LAB_DB.PUBLIC;
+
+/*-----------------------------------------------------------------------------------------
+Once the data is loaded into production tables, we now in this file create views required 
+for visualization
+------------------------------------------------------------------------------------------*/
+
+
