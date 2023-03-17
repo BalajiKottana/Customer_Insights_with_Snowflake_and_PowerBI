5c2e864ef4bd11c046032936230ed89e32d78b66
Balaji Kottana <122858293+BalajiKottana@users.noreply.github.com>
2023-03-17 17:08:22 -0400

    Update Snowflake_Worksheet_3.sql

--- Snowflake_Worksheet_3.sql
+++ Snowflake_Worksheet_3.sql
@@ -1,9 +1,10 @@
 /*-----------------------------------------------------------------------------------------
-If the previous session is not retained execute the first two sql commands
+If the previous session is not retained execute the first three sql commands
 --------------------------------------------------------------------------------------------*/
 -- USE ROLE system admin to run the script
 USE ROLE SYSADMIN;
 
+USE WAREHOUSE ELT_WH;
 -- Switch default context to the LAB_DB and PUBLIC schema
 USE LAB_DB.PUBLIC;
 
